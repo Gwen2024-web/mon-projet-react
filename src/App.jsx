b8620a0890f70182bfa64d06ea701a09f5a13bb8
Gwendal <gwendallegoff22@gmail.com>
2025-10-17 19:51:19 +0200

    Mise en place des images

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
+import imgCircles from "C:/Users/gwend/OneDrive/Documents/DesktopCircles.png";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-      </p>
-    </>
-  )
+        <div>
+      <img src={imgCircles} 
+           alt="Circle" style={{
+           width: "100%",
+           height: "100%",
+           display: "block",
+           margin: "20px auto",
+           border: "2px solid white",
+           objectFit: "cover", // couvre tout l’écran sans déformation
+        }}/>
+        </div>
+  );
 }
 
 export default App
