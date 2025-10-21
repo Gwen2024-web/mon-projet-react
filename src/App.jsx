3ebea871a90f6c384aa4619282bb3d8be5aedd8f
Gwendal <gwendallegoff22@gmail.com>
2025-10-21 21:16:32 +0200

    TailleBanniere+Programmes

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,35 @@
 
 
-import CirclesBanniere from "C:/Users/gwend/OneDrive/Documents/CirclesBanniere.png";
-import CirclesProgramme1 from "C:/Users/gwend/OneDrive/Documents/CirclesProgramme1.png";
-import CirclesProgramme2 from "C:/Users/gwend/OneDrive/Documents/CirclesProgramme2.png";
-import CirclesProgramme3 from "C:/Users/gwend/OneDrive/Documents/CirclesProgramme3.png";
-import CirclesBonus from "C:/Users/gwend/OneDrive/Documents/CirclesBonus.png";
-import CirclesTriangle from "C:/Users/gwend/OneDrive/Documents/CirclesTriangle.png";
+import CirclesBanniere from "./assets/CirclesBanniere.png";
+import CirclesProgramme1 from "./assets/CirclesProgramme1.png";
+import CirclesProgramme2 from "./assets/CirclesProgramme2.png";
+import CirclesProgramme3 from "./assets/CirclesProgramme3.png";
+import CirclesBonus from "./assets/CirclesBonus.png";
+import CirclesTriangle from "./assets/CirclesTriangle.png";
 import './App.css'
 
 function App() {
 
   return (
-        <div className="image-container">
-      <img src={CirclesBanniere} 
-           alt="Circle" style={{
-           width: "100%",
-           height: "100%",
-           display: "flex",
-           justifyContent: "top",
-           alignItems: "center",
-           margin: "20px auto",
-           border: "2px solid white",
-        }}/>
-        <div className="texte-bas">
-            <h1 className="text-3xl font-bold" style={{
-            color: 'black',
-            }}>Le Week end sportif hasardeux qui ouvre tes cercles</h1>
-      </div>
-      <img src={CirclesProgramme1}
-           alt="Circle" style={{}}/>
-      <img src={CirclesProgramme2}
-           alt="Circles" style={{}}/>
-      <img src={CirclesProgramme3}
-           alt="Circles" style={{}}/>
-      <img src={CirclesBonus}
-           alt="Circles" style={{}}/>
-      <img src={CirclesTriangle}
-           alt="Circles" style={{}}/>
+     <>
+        <header className="banniere"></header>
+
+        <div className="title-section">
+            <h1 className="page-title">Le Week end sportif hasardeux qui ouvre tes cercles</h1>
         </div>
+
+        <section className="programmes">
+        <img src={CirclesProgramme1} alt="Programme 1" className="image" />
+        <img src={CirclesProgramme2} alt="Programme 2" className="image" />
+        <img src={CirclesProgramme3} alt="Programme 3" className="image" />
+      </section>
+
+      {/* Autres images */}
+      <section className="extras">
+        <img src={CirclesBonus} alt="Bonus" className="extra-image" />
+        <img src={CirclesTriangle} alt="Triangle" className="extra-image" />
+      </section>
+      </>
   );
 }
 
