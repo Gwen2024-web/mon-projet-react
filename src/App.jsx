19664afad31def0e81fa2a39ddc6ece13714205b
Gwendal <gwendallegoff22@gmail.com>
2025-11-10 14:44:05 +0100

    Changements mobile programme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 
+
+
 import CirclesBanniere from "./assets/CirclesBanniere.png";
 import CirclesProgramme1 from "./assets/CirclesProgramme1.png";
 import CirclesProgramme2 from "./assets/CirclesProgramme2.png";
@@ -14,7 +16,8 @@ function App() {
     <>
       {/* === Bannière principale === */}
       <header
-        className="banniere flex flex-row justify-center items-center max-md:flex-col"
+        className="banniere flex flex-row justify-center items-center max-md:flex-col
+         max-md:h-[300px] max-md:bg-contain max-md:bg-center max-md:bg-no-repeat"
         style={{ backgroundImage: `url(${CirclesBanniere})` }}
       ></header>
 
@@ -32,7 +35,7 @@ function App() {
       <img
         src={RectangleCircles}
         alt="rect"
-        className="block mx-auto w-[98%] max-md:w-[90%] h-auto max-md:top-[200px] max-md:right-[250px] max-md:w-[600px]"
+        className="block mx-auto w-[98%] max-md:w-[90%] h-auto"
       />
 
       {/* === Programme === */}
@@ -40,21 +43,21 @@ function App() {
         Le Programme
       </h1>
 
-      <section className="programmes flex justify-center items-center flex-wrap gap-10 mt-8">
+      <section className="programmes flex flex-wrap justify-center gap-10 mt-8">
         <img
           src={CirclesProgramme1}
           alt="Programme 1"
-          className="image w-[500px] max-md:w-[300px]"
+          className="block mx-auto w-full max-w-[600px] h-auto rounded-xl transition-transform duration-300 ease-in-out"
         />
         <img
           src={CirclesProgramme2}
           alt="Programme 2"
-          className="image w-[500px] max-md:w-[300px]"
+          className="block mx-auto w-full max-w-[600px] h-auto rounded-xl transition-transform duration-300 ease-in-out"
         />
         <img
           src={CirclesProgramme3}
           alt="Programme 3"
-          className="image w-[500px] max-md:w-[300px]"
+          className="block mx-auto w-full max-w-[600px] h-auto rounded-xl transition-transform duration-300 ease-in-out"
         />
       </section>
 
@@ -63,33 +66,33 @@ function App() {
         <img
           src={RectangleCircles}
           alt="rect"
-          className="block mx-auto w-[98%] max-md:w-[90%] h-auto max-md:top-[200px] max-md:right-[250px] max-md:w-[600px]"
+          className="block mx-auto w-[98%] max-md:w-[90%] h-auto"
         />
         <h1 className="page-title mt-6 h-auto max-md:top-[200px] max-md:right-[250px] max-md:w-[600px]">À propos</h1>
 
-        <div className="absolute top-[240px] right-[1280px] h-auto max-md:top-[200px] max-md:right-[250px] max-md:w-[10px]">
-          <h1 className="bonus-texte text-2xl max-md:text-lg static max-md:mt-4">
+        <div className="relative flex flex-col items-center text-center mt-[150px] mr-[1280px] max-md:translate-y-[-150px] max-md:translate-x-[50px]">
+          <h1 className="bonus-texte text-base sm:text-lg md:text-2xl lg:text-2xl xl:text-3xl lg:text-2xl">
             Offre une place à tes potes !
           </h1>
         </div>
 
-        <div className="absolute top-[850px] w-[900px] right-[480px] h-auto">
-          <h1 className="contact-texte1 text-2xl max-md:w-[2600px]">
+        <div className="relative flex flex-col items-center text-center mt-[600px] ml-[10px] max-md:mt-[180px] max-md:translate-x-[-160px]">
+          <h1 className="contact-texte1 text-base sm:text-lg md:text-2xl lg:text-2xl xl:text-3xl lg:text-2xl">
             Nous contacter : - Mail : maxime.georges056@gmail.com
           </h1>
-          <h1 className="contact-texte2 text-2xl max-md:w-[2600px] ml-[-57px] mt-[10px]">
+          <h1 className="contact-texte2 text-base text-base sm:text-lg md:text-2xl lg:text-2xl xl:text-3xl mr-[105px] mt-[10px] max-md:translate-x-[21px]">
             - Réseaux : DM
           </h1>
         </div>
         <img
           src={CirclesBonus}
           alt="Bonus"
-          className="absolute top-[300px] right-[1380px] w-[500px] h-auto max-md:top-[200px] max-md:right-[250px] max-md:w-[350px]"
+          className="absolute top-[300px] right-[1380px] w-[500px] h-auto max-md:top-[200px] max-md:right-[1075px] max-md:w-[250px]"
         />
         <img
           src={CirclesTriangle}
           alt="Triangle"
-          className="absolute top-[60px] left-[1350px] w-[560px] h-auto max-md:top-[200px] max-md:left-[400px] max-md:w-[350px]"
+          className="absolute top-[60px] left-[1350px] w-[560px] h-auto max-md:top-[95px] max-md:left-[380px] max-md:w-[250px]"
         />
       </section>
     </>
