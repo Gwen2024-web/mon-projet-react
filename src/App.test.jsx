c066844e7aa3d395586ce6ecdde86e1bf753e230
Gwen2024-web <gwendallegoff22@gmail.com>
2026-08-01 01:30:10 +0200

    Changes to test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,10 +6,9 @@ import App from "./App";
 test("affiche les contenus principaux de la page Circles", () => {
   render(<App />);
 
-  expect(screen.getByRole("heading", { name: /le week-end sportif.*agrandit tes cercles/i })).toBeInTheDocument();
+  expect(screen.getByRole("heading", { name: /le week-end sportif qui agrandit tes cercles/i })).toBeInTheDocument();
   expect(screen.getByTestId("main-video")).toBeInTheDocument();
-  expect(screen.getByRole("heading", { name: /trois jours.*se rencontrer/i })).toBeInTheDocument();
+  expect(screen.getByText("heading", { name: /trois jours.*se rencontrer/i })).toBeInTheDocument();
   expect(screen.getByText(/venir seul/i)).toBeInTheDocument();
-  expect(screen.getByRole("link", { name: /maxime.georges/i })).toBeInTheDocument();
   expect(screen.getAllByRole("img").length).toBeGreaterThan(0);
 });
